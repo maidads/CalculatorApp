6d8c902618f505b96168d58d166ecf4f9fea8e27
Maida <98819095+maidads@users.noreply.github.com>
2024-02-13 16:07:06 +0100

    added functions with empty body to do when pressing the buttons

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onClearClick(view: View) {}
+    fun onBackClick(view: View) {}
+    fun onOperatorClick(view: View) {}
+    fun onDigitClick(view: View) {}
+    fun onAllClearClick(view: View) {}
+    fun onEqualClick(view: View) {}
 
 
 }
